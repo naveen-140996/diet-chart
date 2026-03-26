4d66407fb46cb7a7d1d4b34973422d0a845caa40
Naveen Vijayan <navinbs0614@gmail.com>
2026-03-27 00:05:43 +0530

    adding

--- server.js
+++ server.js
@@ -31,6 +31,7 @@ app.use(cors({
 
 app.use(cookieParser());
 app.use("/api/auth", require("./routes/authRoutes"));
+app.use("/api/user", require("./routes/userRoutes"));
 
 
 const PORT = process.env.PORT || 5000;
