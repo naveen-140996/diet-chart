4326269aa8bc920b0a7bbc35826c379e2ca3dce6
Naveen Vijayan <navinbs0614@gmail.com>
2026-03-27 00:18:09 +0530

    needed

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,7 +3,13 @@ const router = express.Router();
 const { protect } = require("../middleware/authMiddleware");
 
 router.get("/profile", protect, (req, res) => {
-  res.json({ user: req.user });
+  res.json({
+    user: {
+      id: req.user._id, // ✅ clean id
+      name: req.user.name,
+      email: req.user.email,
+    },
+  });
 });
 
 module.exports = router;
