ad79a759542eff5df7a97354c77ba87cfb440ab2
Naveen Vijayan <navinbs0614@gmail.com>
2026-03-26 23:01:59 +0530

    new update

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const { signup, login } = require("../controller/authController");
 
 router.post("/signup", signup);
-router.post("/", login); 
+router.post("/login", login); 
 
 module.exports = router;
