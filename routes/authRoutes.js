382130141a4760f5906247defc6c23e93934b5c6
Naveen Vijayan <navinbs0614@gmail.com>
2026-03-26 21:52:54 +0530

    send this

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const { signup, login } = require("../controller/authController");
 
 router.post("/signup", signup);
-router.post("/login", login); 
+router.post("/", login); 
 
 module.exports = router;
